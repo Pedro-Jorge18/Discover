cb4ed59afda36eda34064ae77a39025b77e2cbed
hiagoPilar <hiagopilar@icloud.com>
2025-10-21 20:53:08 +0100

    feat: config UserModel, sanctum.php, kernel and create requests folders

---
+++ back-end/Discover/app/Http/Requests/Users/Auth/RegisterUserRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Users\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true; //permited everyone register
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'phone' => 'required|string|max:20|unique:users',
+            'birthday' => 'required|date|before:today|after:1900-01-01',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'gender' => 'nullable|in:M,W,O',
+            'language' => 'sometimes|in:pt,en,es',
+            'about' => 'nullable|string|mas:500',
+        ];
+    }
+}
