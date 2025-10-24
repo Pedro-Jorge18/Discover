547387732611382a1de7c0157952b770b3e64b52
Luan Rodrigues <luanrwork@gmail.com>
2025-10-24 23:23:05 +0100

    Form Request Property incluido

--- back-end/Discover/app/Http/Requests/StorePropertyRequest.php
+++ back-end/Discover/app/Http/Requests/StorePropertyRequest.php
@@ -11,7 +11,7 @@ class StorePropertyRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,59 @@ class StorePropertyRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            //informações iniciais ( OBRIGATORIAS )
+
+            'title' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
+            'summary' => 'required|string|max:255',
+            'price_per_night' => 'required|numeric|min:1',
+
+            // lOCALIZÇÃO
+            'address' => 'required|string|max:255',
+            'neighborhood' => 'required|string|max:255',
+            'postal_code' => 'required|string|max:25',
+            'city_id' => 'required|integer|exists:cities,id',
+            'latitude' => 'required|numeric|min:-90|max:90',
+            'longitude' => 'required|numeric|min:-180|max:180',
+
+            //CARACTERISTICAS
+            'property_type_id' => 'required|integer|exists:property_types,id',
+            'listing_type_id' => 'required|integer|exists:listing_types,id',
+            'max_guests' => 'required|integer|min:1',
+            'bedrooms' => 'required|integer|min:0',
+            'beds' => 'required|integer|min:1',
+            'bathrooms' => 'required|integer|min:0',
+
+            //CONFIGURAÇÃO
+            'cleaning_fee' => 'sometimes|numeric|min:0',
+            'service_fee' => 'sometimes|numeric|min:0',
+            'security_deposit' => 'sometimes|numeric|min:0',
+            'area' => 'sometimes|numeric|min:0',
+            'floor' => 'sometimes|numeric|min:0',
+            'min_nights' => 'sometimes|numeric|min:1',
+            'max_nights' => 'sometimes|numeric|min:1',
+
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+              //messagens de obrigação
+              'title.required' => 'It has to have a title.',
+              'description.required' => 'It has to have a description.',
+              'summary.required' => 'It has to have a summary.',
+              'price_per_night.required' => 'The minimum allowed is one nigh.',
+              'address.required' => 'Address is required',
+              'neighborhood.required' => 'Neighborhood is required',
+              'postal_code.required' => 'Postal code is required',
+              'city_id.required' => 'City is required',
+              'property_type_id.required' => 'Property Type is required',
+              'listing_type_id.required' => 'Listing Type is required',
+              'max_guests.required' => 'Maximum number of guests is required',
+              'bedrooms.required' => 'Number of bedrooms is required',
+              'beds.required' => 'Number of bedrooms is required',
+              'bathrooms.required' => 'Number of bathrooms is required',
         ];
     }
 }
