82d53026d4722ac8e9fc9202e416dcb1a94694bc
hiagoPilar <hiagopilar@icloud.com>
2025-11-04 18:39:13 +0000

    CreatePaymentRequest and lang/payment

---
+++ back-end/Discover/app/Http/Requests/Payment/CreatePaymentRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Payment;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+
+class CreatePaymentRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+        //if you want, can add check if the user is owner of reservation
+    }
+
+
+    public function rules(): array
+    {
+        return [
+            'reservation_id' => ['required', 'integer', 'exists:reservation_id'],
+            'amount' => ['required', 'numeric', 'min:1'],
+            'currency' => ['nullable', 'string', 'size:3'],
+            'payment_gateway' => ['nullable', 'string', 'in:stripe,paypal'],
+            'payment_method_id' => ['nullable', 'integer', 'exists:payment_methods,id'],
+            'description' => ['nullable', 'string', 'max:255'],
+            'metadata' => ['nullable', 'array'],
+
+        ];
+    }
+}
---
+++ back-end/Discover/lang/en/payment.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'reservation' => [
+        'id_required' => 'The reservation is required.',
+        'id_exists' => 'The reservation does not exists.',
+    ],
+
+    'amount' => [
+        'required' => 'The payment amount is mandatory.',
+        'numeric' => 'The payment amount must be numerical.',
+        'min' => 'The minimum allowed value is 1.'
+    ],
+
+];
