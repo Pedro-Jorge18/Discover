c694fd111ed6ec5d0d542c79f2b639a05eb8e630
hiagoPilar <hiagopilar@icloud.com>
2025-11-04 21:35:07 +0000

    Routes

--- back-end/Discover/routes/api.php
+++ back-end/Discover/routes/api.php
@@ -3,7 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\Webhook\StripeWebHookController;
 
+//authentication
 Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register'])->middleware('throttle:5,1');
     Route::post('login', [AuthController::class, 'login'])->middleware('throttle:10,1');
@@ -12,3 +15,22 @@ Route::prefix('auth')->group(function () {
         Route::get('me', [AuthController::class, 'me']);
     });
 });
+
+
+//payment
+Route::prefix('payments')->middleware([auth::sanctum])->group(function () {
+    //create checkout
+    Route::post('/', [PaymentController::class, 'store'])->name('payments.store');
+
+    //see payment details
+    Route::get('/{payment}', [PaymentController::class, 'show'])->name('payments.show');
+
+    //refund
+    Route::post('/{payment}/refund', [PaymentController::class, 'refund'])->name('payments.refund');
+
+    //list authenticated user payments
+    Route::get('/', [PaymentController::class, 'index'])->name('payments.index');
+});
+
+//webhook
+ROute::post('/webhook/stripe', [StripeWebHookController::class, 'handle'])->name('webhook.stripe');
