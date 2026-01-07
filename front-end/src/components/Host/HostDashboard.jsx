17f2336870fbabd8152304025dfcd8525c3877b0
Rochinhaa <goncalorocha5@gmail.com>
2026-01-07 22:22:15 +0000

    Final

--- front-end/src/components/Host/HostDashboard.jsx
+++ front-end/src/components/Host/HostDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+  import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../Nav/Header.jsx';
 import PropertyFormModal from './PropertyFormModal.jsx';
